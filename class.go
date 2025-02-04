ad4d598f62b6fe9a33d0ef1a5f24f3a6b39da76f
Will2406 <william.quispe2497@icloud.com>
2025-02-03 20:05:53 -0500

    Crear struct y metodos apuntando a su valor en memoria para modificarlo

---
+++ class.go
@@ -0,0 +1,14 @@
+package main
+
+type Employee struct {
+	id   int
+	name string
+}
+
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
