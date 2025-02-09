8933dd170b9eaeed5047e87d0ec7f083b5f1565c
Will2406 <william.quispe2497@icloud.com>
2025-02-09 15:40:56 -0500

    Apply goroutines to tcp connection

--- net/port.go
+++ net/port.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan it")
+
 func main() {
-	for i := 0; i < 100; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+
+	flag.Parse()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
+
+// go run net/port.go --site=scanme.webscantest.com
